c570f4bffcc145804b88c82b36d074003e8473af
Gabriel Moskwa <gabriel.moskwa@wp.pl>
2023-05-23 22:32:06 +0200

    api + navigation

---
+++ app/src/main/java/com/example/hangmangame/SecondFragment.kt
@@ -0,0 +1,83 @@
+package com.example.hangmangame
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hangmangame.databinding.FragmentSecondBinding
+import okhttp3.*
+import java.io.IOException
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+
+    private var randomWord: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        Log.i("SecondFragment", "t0")
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("SecondFragment", "t1")
+        // Pobieranie losowego słowa z API
+        fetchRandomWord()
+        Log.i("SecondFragment", "t2")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun fetchRandomWord() {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://random-word-api.herokuapp.com/word?number=1")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Obsługa błędu pobierania danych z API
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body?.string()
+                randomWord = extractRandomWord(responseData)
+
+                // Logowanie pobranego słowa
+                randomWord?.let {
+                    val wordLength = it.length
+                    Log.i("SecondFragment", "Pobrane słowo: $it")
+                    Log.i("SecondFragment", "Pobrane słowo długość: $wordLength")
+                    val underscoreText = "_ ".repeat(wordLength)
+                    activity?.runOnUiThread {
+                        binding.generatedWord.text = underscoreText
+                        binding.generatedWord.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
+    }
+
+    private fun extractRandomWord(responseData: String?): String? {
+        // Wyodrębnij losowe słowo z odpowiedzi API
+        val regex = """\["(\w+)"\]""".toRegex()
+        val matchResult = regex.find(responseData ?: "")
+        return matchResult?.groupValues?.get(1)
+    }
+}
